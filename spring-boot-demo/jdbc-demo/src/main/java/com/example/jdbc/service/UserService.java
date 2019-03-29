59852e20d7847bfe75d56dd7168529c76b21d92b
sunshineSeay <liu.hao813@foxmail.com>
2019-03-29 15:34:22 +0800

    JdbcTemplate

---
+++ spring-boot-demo/jdbc-demo/src/main/java/com/example/jdbc/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.jdbc.service;
+
+public interface UserService {
+
+	/**
+	 * 新增一个用户
+	 * @param name
+	 * @param age
+	 */
+	void create(String name, Integer age);
+
+	/**
+	 * 根据name删除一个用户高
+	 * @param name
+	 */
+	void deleteByName(String name);
+
+	/**
+	 * 获取用户总量
+	 */
+	Integer getUserCount();
+
+
+	void deleteAllUsers();
+}
